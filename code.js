5779de61bb197c176bd1a0a97eb8e56b98c65a3c
MrAnonymous4 <dakshcoc123@gmail.com>
2021-01-15 11:36:44 +0530

    code

---
+++ code.js
@@ -0,0 +1,11 @@
+  function goto(){
+  var a=document.getElementById("inp").value;
+
+  if (a=="#42H26@") {
+    window.location.href=("https://gleam.io/bSZM0/1000-giveaway");
+  }
+  else {
+  	alert("ALERT : The code you have entered is wrong, please check it.")
+
+  }
+}
